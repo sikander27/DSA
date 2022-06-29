d0052035ee36d2e288a3010b05a5f370537c4a3c
sikander27 <imsikandarkhan@outlook.com>
2022-06-29 23:05:53 +0530

    Added bubble sort algo (improved)

--- Sorting-Algorithm/bubble-sort.py
+++ Sorting-Algorithm/bubble-sort.py
@@ -1,4 +1,10 @@
 # Bubble sort in Python
+# Time Complexity	 
+# Best	O(n)
+# Worst	O(n2)
+# Average	O(n2)
+# Space Complexity	O(1)
+# Stability	Yes
 
 def bubbleSort(array):
     
@@ -6,9 +12,8 @@ def bubbleSort(array):
   for i in range(len(array)):
 
     # loop to compare array elements
-    print(i)
+    swapped = False
     for j in range(0, len(array) - i - 1):
-      print(i, j)
       # compare two adjacent elements
       # change > to < to sort in descending order
       if array[j] > array[j + 1]:
@@ -19,6 +24,11 @@ def bubbleSort(array):
         array[j] = array[j+1]
         array[j+1] = temp
 
+        swapped = True
+
+    # If array is not swapped, it means it's already sorted
+    if not swapped:
+        break
 
 data = [-2, 45, 0, 11, -9]
 
